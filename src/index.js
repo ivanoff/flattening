dc17cb4d51e60e52eec520c3847e02b4ea61539d
Dimitry Ivanov <2@ivanoff.org.ua>
2022-02-20 22:38:26 +0200

    add git repository

---
+++ src/index.js
@@ -0,0 +1,16 @@
+module.exports = function run(obj, k = '', rr = {}) {
+  const prefix = k || '';
+  if(Array.isArray(obj)) {
+    for (let i = 0; i < obj.length; i++) {
+      run(obj[i], `${prefix}${i}`, rr);
+    }
+  } else if(typeof obj === 'object' && obj !== null) {
+    for (let key in obj) {
+      const k = key.includes('.')? `"${key}"` : key;
+      run(obj[key], `${prefix}${k}`, rr);
+    }
+  } else {
+    rr[k] = obj;
+  }
+  return rr;
+};
